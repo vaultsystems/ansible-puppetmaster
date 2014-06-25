4c20db127b1ae9d2e423aa9678748dc771cfe5d1
Christoph Dwertmann <cdwertmann@gmail.com>
2014-06-25 16:34:35 +1000

    add puppet_dns script

---
+++ templates/usr/local/bin/puppet_dns.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+entries = {}
+hosts_file = "/etc/hosts_puppet"
+
+if File.exists?(hosts_file)
+  hosts = File.open(hosts_file, "r")
+  while (line = hosts.gets)
+    ip, *host = line.split(" ")
+    entries[ip]=host
+  end
+  hosts.close
+end
+
+hosts = File.open(hosts_file, "w+")
+puppet_log = File.open("/var/log/puppet/masterhttp.log", "r")
+
+while (line = puppet_log.gets)
+  if line.include? "GET /production/node/"
+    ip = line.match('(?:[0-9]{1,3}\.){3}[0-9]{1,3}').to_s
+    host = line.match('node\/(.*)\?').captures.first
+    entries[ip] = [host]
+  end
+end
+
+entries.each {|ip, host| 
+  hosts.write("#{ip} #{host.join(' ')}\n")
+}
+
+hosts.close
+puppet_log.close
+
+system "killall -HUP dnsmasq"
