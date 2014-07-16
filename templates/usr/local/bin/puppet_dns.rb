f7bce7aac092ff2eea3c137e1ecc4f910b221828
Christoph Dwertmann <cdwertmann@gmail.com>
2014-07-17 00:49:06 +1000

    use passenger to serve puppet

--- templates/usr/local/bin/puppet_dns.rb
+++ templates/usr/local/bin/puppet_dns.rb
@@ -13,16 +13,21 @@ if File.exists?(hosts_file)
 end
 
 hosts = File.open(hosts_file, "w+")
-puppet_log = File.open("/var/log/puppet/masterhttp.log", "r")
+puppet_log = File.open("/var/log/apache2/other_vhosts_access.log", "r")
 
 while (line = puppet_log.gets)
   if line.include? "GET /production/node/"
-    ip = line.match('(?:[0-9]{1,3}\.){3}[0-9]{1,3}').to_s
+    # puppet log:
+    # ip = line.match('(?:[0-9]{1,3}\.){3}[0-9]{1,3}').to_s
+    # apache log:
+    ip = line.match(' ((?:[0-9]{1,3}\.){3}[0-9]{1,3})').captures.first.to_s
     host = line.match('node\/(.*)\?').captures.first
     entries.delete_if {| key, value | value==host }
     entries[ip] = host
   end
 end
+
+entries = entries.sort_by { |ip, host| host }
 
 entries.each {|ip, host|
   hosts.write("#{ip} #{host}\n")
